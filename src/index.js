ce5947dd579dcfe64ad5d35e391ad3e3c3097224
Geronymos <spielerbereich@gmail.com>
2020-12-31 02:58:17 +0100

    tensorflow/mediapipe hand pose detection

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import "aframe";
 
-
+// import "./components/hand-pose-detection-controler";
+// import "./mediapipe-pose";
+import "./tensorflow-pose";
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devtool: 'source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public')
     }
