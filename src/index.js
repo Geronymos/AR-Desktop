aea31388cf4958f6d4e07d69c0ab1774cebc9649
Geronymos <spielerbereich@gmail.com>
2021-04-05 20:23:17 +0200

    ➕ basic Three.js Scene

--- src/index.js
+++ src/index.js
@@ -1,13 +1,53 @@
-import "aframe";
-import "aframe-event-set-component";
-/* import "super-hands";
-import "aframe-physics-system/dist/aframe-physics-system";
-import "aframe-physics-extras"; */
+import * as THREE from 'three';
+import * as Stats from 'stats.js';
 
-import "./components/video-background";
-import "./components/browser";
-import "./components/tabCapture";
-import "ar.js";
+// import "ar.js";
 
-window.addEventListener("arjs-video-loaded", e => document.querySelector("a-scene").setAttribute("video-background", "#arjs-video"));
+// window.addEventListener("arjs-video-loaded", e => document.querySelector("a-scene").setAttribute("video-background", "#arjs-video"));
 // window.addEventListener("arjs-video-loaded", e => document.querySelector("a-scene").emit("arjsvideoloaded"));
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const stats = new Stats();
+stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild( stats.dom );
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+window.addEventListener( 'resize', onWindowResize );
+
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    render();
+};
+
+function render() {
+    renderer.render(scene, camera);
+    stats.update();
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+animate();
